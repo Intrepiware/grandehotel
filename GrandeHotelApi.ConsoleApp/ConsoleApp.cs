17cb162b9e260f2886beb5c06b58c3cf1fd311da
Mike Devlin <mike.devlin@tigerpistol.com>
2019-10-11 15:53:56 -0500

    Add Console App

---
+++ GrandeHotelApi.ConsoleApp/ConsoleApp.cs
@@ -0,0 +1,41 @@
+﻿using GrandeHotel.Lib.Data.Models;
+using GrandeHotel.Lib.Data.Services;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrandeHotelApi.ConsoleApp
+{
+    class ConsoleApp
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ConsoleApp(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task Run()
+        {
+            // Get the initial list of rooms
+            var rooms = await _unitOfWork.Rooms.GetAll();
+            Console.WriteLine("Initial list:");
+            rooms.ToList().ForEach(r => Console.WriteLine($"{r.RoomId}: {r.Name}"));
+            Console.WriteLine();
+
+            // Add a new room
+            Room room = new Room
+            {
+                Name = $"New room {Guid.NewGuid().ToString("n").Substring(0, 6)}",
+                NightlyRate = 101
+            };
+            await _unitOfWork.Rooms.Add(room);
+            await _unitOfWork.Complete();
+
+            // Get the final list of rooms
+            rooms = await _unitOfWork.Rooms.GetAll();
+            Console.WriteLine("Final list:");
+            rooms.ToList().ForEach(r => Console.WriteLine($"{r.RoomId}: {r.Name}"));
+            Console.WriteLine();
+        }
+    }
+}
---
+++ GrandeHotelApi.ConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using GrandeHotel.Lib.Data.Models;
+using GrandeHotel.Lib.Data.Services;
+using GrandeHotel.Lib.Data.Services.Impl;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+
+namespace GrandeHotelApi.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IServiceCollection services = ConfigureServices();
+
+            ServiceProvider provider = services.BuildServiceProvider();
+
+            provider.GetService<ConsoleApp>().Run().Wait();
+        }
+
+        static IServiceCollection ConfigureServices()
+        {
+            IServiceCollection serviceProvider = new ServiceCollection();
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+#if DEBUG
+                .AddJsonFile("appSettings.Development.json", optional: true)
+#endif
+                .AddEnvironmentVariables();
+
+            IConfigurationRoot config = builder.Build();
+
+            serviceProvider.AddDbContext<GrandeHotelCustomContext>(options => options.UseSqlServer(config.GetConnectionString("grande_hotel")));
+            serviceProvider.AddTransient<IUnitOfWork, UnitOfWork>();
+            serviceProvider.AddTransient<ConsoleApp>();
+            return serviceProvider;
+        }
+    }
+}

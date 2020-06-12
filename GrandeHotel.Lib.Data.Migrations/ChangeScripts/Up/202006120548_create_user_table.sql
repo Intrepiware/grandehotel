0cd4e5ea40239770abc2de1b0882fd82523360c8
Intrepiware <mike@intrepiware.com>
2020-06-12 06:16:26 -0500

    Add user table (#18)
    * Add user table
    
    * Add default constraint to user.create_date
    
    Co-authored-by: Mike Devlin <mike@epay3.com>

---
+++ GrandeHotel.Lib.Data.Migrations/ChangeScripts/Up/202006120548_create_user_table.sql
@@ -0,0 +1,14 @@
+﻿create table reservations.[user] (
+	[user_id] int not null identity,
+	[email] varchar(255) not null,
+	[password] varchar(255) not null,
+	[first_name] varchar(100) not null,
+	[last_name] varchar(100) not null,
+	[create_date] datetime not null
+);
+
+alter table reservations.[user]
+add constraint pk_user primary key (user_id);
+
+alter table reservations.[user]
+add constraint uq_user_email unique(email);

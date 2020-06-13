f77a9ff1732562c28b130ce5a224809816385c17
Intrepiware <mike@intrepiware.com>
2020-06-12 21:19:17 -0500

    19 di configuration (#21)
    * Add development/production IOC config

---
+++ GrandeHotelApi/Services/IOC.cs
@@ -0,0 +1,43 @@
+﻿using GrandeHotel.Lib.Data.Services;
+using GrandeHotel.Lib.Data.Services.Impl;
+using GrandeHotelApi.Models.Auth;
+using GrandeHotelApi.Services.Impl;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace GrandeHotelApi.Services
+{
+    public static class IOC
+    {
+        public static IServiceCollection Register(IServiceCollection collection, IConfiguration configuration, bool isDevelopment)
+        {
+            collection = Base(collection, configuration);
+            if (isDevelopment)
+                collection = Development(collection);
+            else
+                collection = Production(collection);
+
+            return collection;
+        }
+
+        private static IServiceCollection Development(IServiceCollection services)
+        {
+            return services;
+        }
+
+        private static IServiceCollection Production(IServiceCollection services)
+        {
+            return services;
+        }
+
+        private static IServiceCollection Base(IServiceCollection services,
+            IConfiguration config)
+        {
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddSingleton<ITokenService, OktaTokenService>();
+            services.Configure<OktaSettings>(config.GetSection("Okta"));
+            return services;
+        }
+    }
+}

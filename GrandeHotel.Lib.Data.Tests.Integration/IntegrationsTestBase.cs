1b11b6c6e975eeae870c1fdcc79591600cf3806a
Mike Devlin <mike@intrepiware.com>
2021-06-23 15:18:41 -0500

    More misc. fixes

--- GrandeHotel.Lib.Data.Tests.Integration/IntegrationsTestBase.cs
+++ GrandeHotel.Lib.Data.Tests.Integration/IntegrationsTestBase.cs
@@ -16,7 +16,6 @@ namespace GrandeHotel.Lib.Data.Tests.Integration
 {
     public class IntegrationsTestBase
     {
-        // private const string ContentRootLocation = "..\\..\\..\\..\\TigerPistol.Video";
         public const string TestConnectionString = "Server=(local);Database=GrandeHotel_test;Trusted_Connection=True;";
         public const string RoundhouseStartLocation = "..\\..\\..\\..\\GrandeHotel.Lib.Data.Migrations\\bin\\debug\\netstandard2.1";
         public const string TestDataLocation = "TestSqlScripts";
